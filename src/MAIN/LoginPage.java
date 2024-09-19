75df20d3514819993a80550018ef3a2d91ed7eca
Atharva <atharvabapat123@gmail.com>
2024-09-19 18:06:37 +0530

    Login Page Updated Again

--- src/MAIN/LoginPage.java
+++ src/MAIN/LoginPage.java
@@ -3,13 +3,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 public class LoginPage extends JFrame {
 
     private JTextField usernameField;
     private JPasswordField passwordField;
     private JButton loginButton;
-    private JLabel backgroundLabel;
+    private JLabel backgroundLabel,l1,l2;
 
     public LoginPage() {
         // Set up the frame
@@ -24,8 +25,9 @@ public class LoginPage extends JFrame {
 
         // Create a panel for the login form
         JPanel loginPanel = new JPanel();
+        loginPanel.setBackground(Color.white);
         loginPanel.setLayout(new GridBagLayout());
-        loginPanel.setOpaque(false); // Make the panel transparent to allow the background to show through
+        loginPanel.setOpaque(true); // Make the panel transparent to allow the background to show through
 
         // Create a GridBagConstraints for the login form components
         GridBagConstraints gbc = new GridBagConstraints();
@@ -35,7 +37,10 @@ public class LoginPage extends JFrame {
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.anchor = GridBagConstraints.LINE_END;
-        loginPanel.add(new JLabel("Username:"), gbc);
+
+        l1= new JLabel("Username:");
+        loginPanel.add(l1, gbc);
+        
 
         gbc.gridx = 1;
         gbc.gridy = 0;
@@ -47,8 +52,8 @@ public class LoginPage extends JFrame {
         gbc.gridx = 0;
         gbc.gridy = 1;
         gbc.anchor = GridBagConstraints.LINE_END;
-        loginPanel.add(new JLabel("Password:"), gbc);
-
+        l2= new JLabel("Password:");
+        loginPanel.add(l2, gbc);
         gbc.gridx = 1;
         gbc.gridy = 1;
         gbc.anchor = GridBagConstraints.LINE_START;
@@ -60,7 +65,12 @@ public class LoginPage extends JFrame {
         gbc.gridy = 2;
         gbc.anchor = GridBagConstraints.LINE_START;
         loginButton = new JButton("Login");
-        loginButton.setForeground(Color.RED); // Set the login button text color to red
+        l1.setForeground(Color.green);
+        l2.setForeground(Color.green);
+        Border border = new LineBorder(new Color(179, 245, 183), 4, true);
+        loginPanel.setBorder(border);
+        loginButton.setBackground(Color.green);
+        loginButton.setForeground(Color.black); // Set the login button text color to red
         loginButton.addActionListener(new LoginButtonListener());
         loginPanel.add(loginButton, gbc);
 
